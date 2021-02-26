20a00f09f39929544933d0920474d06a4988aa52
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-27 01:24:05 +0600

    added react fontawsome

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 const Product = (props) => {
     const {img, name, seller, price, stock} = props.product;
@@ -12,7 +14,8 @@ const Product = (props) => {
                 <br/>
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
-                <p><small>Only {} left in stock - order soon</small></p>
+                <p><small>Only {stock} left in stock - order soon</small></p>
+                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart}/> Add to cart</button>
             </div>
         </div>
     );
