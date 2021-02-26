5b9147ee07ae6af5537b6d8719f8429f0dffdcfd
aar-atiq <aar.msdeveloper01@gmail.com>
2021-02-27 00:12:05 +0600

    align item in product component

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Product.css';
 const Product = (props) => {
+    const {img, name, seller, price, stock} = props.product;
     return (
-        <div>
-            <h1> {props.product.name}</h1>
+        <div className="product">
+            <div>
+                <img src={img} alt=""/>
+            </div>
+            <div className="product-name">
+                <h4>{name}</h4>
+                <br/>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {} left in stock - order soon</small></p>
+            </div>
         </div>
     );
 };
